1b04fcf6646125b23b9ada5e2bf2542c02e5317b
Marty Jones <mj8263788@gmail.com>
2023-04-16 15:47:04 -0400

    atf: update atf to snapshot support rk3568 - https://git.trustedfirmware.org//TF-A%2Ftrusted-firmware-a.git/commit/?id=f6f5b5576c158f24930f1dd3036310fe1a8e401c

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ AARCH64_TOOLCHAIN_FILENAME=gcc-arm-${AARCH64_TOOLCHAIN_VERSION}-x86_64-aarch64-n
 AARCH64_TOOLCHAIN_URL=https://developer.arm.com/-/media/Files/downloads/gnu-a/${AARCH64_TOOLCHAIN_VERSION}/binrel/${AARCH64_TOOLCHAIN_FILENAME}
 AARCH64_TOOLCHAIN_SHA256SUM=6f74b1ee370caeb716688d2e467e5b44727fdc0ed56023fe5c72c0620019ecef
 
-ATF_SOURCE_VERSION=2.8
-ATF_SOURCE_FILENAME=arm-trusted-firmware-v${ATF_SOURCE_VERSION}.tar.gz
-ATF_SOURCE_URL=https://codeload.github.com/ARM-software/arm-trusted-firmware/tar.gz/v${ATF_SOURCE_VERSION}
-ATF_SOURCE_SHA256SUM=42256fa354f32b09972e72e0570a0f73698785927f93163b1d1308c485fcb4a6
+ATF_SOURCE_VERSION=2.8.4
+ATF_SOURCE_FILENAME=trusted-firmware-a-f6f5b5576c158f24930f1dd3036310fe1a8e401c.tar.gz
+ATF_SOURCE_URL=https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git/snapshot/trusted-firmware-a-f6f5b5576c158f24930f1dd3036310fe1a8e401c.tar.gz
+ATF_SOURCE_SHA256SUM=217ac00e8f39bc8384370163e4e1e4253787a35df78eb74e9e05a456d6981b22
 # Set fixed build timestamp for reproducible builds
-ATF_BUILD_EPOCH=1587398203
+ATF_BUILD_EPOCH=$(date +%s)
 
 die() {
   echo "$@" 1>&2
--- targets/rk3568.sh
+++ targets/rk3568.sh
@@ -1,4 +1,4 @@
-PLAT=rk3328
+PLAT=rk3568
 TARGET=bl31
 BINARY_PATH=bl31/bl31.elf
 ARCH=aarch64
